11103a697cc396cff685a937cc26b507fea486c9
nagln <laxminagln@gmail.com>
2020-08-18 22:39:18 +0530

    Update Register.js

--- code-bees/src/Register.js
+++ code-bees/src/Register.js
@@ -5,7 +5,7 @@ function Register() {
     <div className="login_page">
     <img src="https://raw.githubusercontent.com/laxminagln/CodeBees/master/cbt.png?token=AIFJEBVN5RX5TTI5DMLJH7C7GTZRY" className="login_image" />
       <form className="login_form">
-        <input type="text" placeholder="Type your email" className="login_email" />
+        <input type="text" placeholder="Type your email..." className="login_email" />
         <input type="password" placeholder="Type your password" className="login_password" />
         <div className="login_details">
         <input type="text" placeholder="Type your name" className="login_name" />
