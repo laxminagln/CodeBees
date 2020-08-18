6b9ea132ff8f5945ffb381931638a60407e47e09
nagln <laxminagln@gmail.com>
2020-08-18 22:38:36 +0530

    Update Login.js

--- code-bees/src/Login.js
+++ code-bees/src/Login.js
@@ -5,7 +5,7 @@ function Login() {
     <div className="login_page">
     <img src="https://raw.githubusercontent.com/laxminagln/CodeBees/master/cbt.png?token=AIFJEBVN5RX5TTI5DMLJH7C7GTZRY" className="login_image" />
       <form className="login_form">
-        <input type="text" placeholder="Type your email" className="login_email" />
+        <input type="text" placeholder="Type your email..." className="login_email" />
         <input type="password" placeholder="Type your password" className="login_password" />
         <div className="login_button">
           <button type="submit" className="login_buttonReg">Register</button>
